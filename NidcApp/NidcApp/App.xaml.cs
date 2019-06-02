1be187372e2225eed5dbf3a90383399f478950a9
Mark Allan <markxa@markxa.com>
2019-06-02 15:49:33 +0100

    Add App Center

--- NidcApp/NidcApp/App.xaml.cs
+++ NidcApp/NidcApp/App.xaml.cs
@@ -1,3 +1,6 @@
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using NidcApp.Pages;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -17,17 +20,16 @@ namespace NidcApp
 
         protected override void OnStart()
         {
+            AppCenter.Start(
+                "ios=979fd95e-807d-4d09-a499-1006fd2d2fee;android=d4106d76-b30d-4c45-9669-cf086c40c848",
+                typeof(Analytics),
+                typeof(Crashes));
+
             AppState.Init();
         }
 
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }
+        protected override void OnSleep() { }
 
-        protected override void OnResume()
-        {
-            AppState.Init();
-        }
+        protected override void OnResume() { }
     }
 }
